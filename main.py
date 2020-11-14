fe45575a123d4382b8870e067e29d34682b386a3
Svenor <lucas.grospart@gmail.com>
2020-11-14 16:44:05 +0100

    PYTHON PATH DE SES MORTS

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-import UI
+import sys
+sys.path.append(r"C:\Users\lucas\Documents\maya\2020\scripts\DeusExGalaxy")
+# Changer par le chemin vers votre dossier ici
+from UI import UI
 
+window = UI()
